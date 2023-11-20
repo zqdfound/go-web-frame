f3237e273c3a01d3ce350f8189c32d62f0a37b21
zhuangqingdian <15950515069@163.com>
2023-11-20 18:20:40 +0800

    geecache

--- cache/lru_test.go
+++ cache/lru_test.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"fmt"
+	"log"
 	"reflect"
 	"testing"
 )
@@ -53,5 +54,40 @@ func TestOnEvicted(t *testing.T) {
 
 	if !reflect.DeepEqual(expect, keys) {
 		t.Fatalf("Call OnEvicted failed, expect keys equals to %s", expect)
+	}
+}
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func TestGetGee(t *testing.T) {
+	loadCounts := make(map[string]int, len(db))
+	gee := NewGroup("scores", 2<<10, GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				if _, ok := loadCounts[key]; !ok {
+					loadCounts[key] = 0
+				}
+				loadCounts[key] += 1
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+
+	for k, v := range db {
+		if view, err := gee.Get(k); err != nil || view.String() != v {
+			t.Fatal("failed to get value of Tom")
+		} // load from callback function
+		if _, err := gee.getter.Get(k); err != nil || loadCounts[k] > 1 {
+			t.Fatalf("cache %s miss", k)
+		} // cache hit
+	}
+
+	if view, err := gee.Get("unknown"); err == nil {
+		t.Fatalf("the value of unknow should be empty, but %s got", view)
 	}
 }
--- mian_test.go
+++ mian_test.go
@@ -2,9 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestPrint(t *testing.T) {
 	fmt.Println("test")
+}
+
+var set = make(map[int]bool, 0)
+var m sync.Mutex
+
+func printOnce(num int) {
+	m.Lock()
+	if _, exist := set[num]; !exist {
+		fmt.Println(num)
+	}
+	set[num] = true
+	m.Unlock()
+}
+
+func TestPrintOnce(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		go printOnce(100)
+	}
+	time.Sleep(time.Second)
 }
